a9506dd65c46a851eaad68663cddd8d94dd1328c
Reinier Hernández <sasuke.reinier@gmail.com>
2025-01-31 16:13:28 -0300

    refactor(ui): improve chat window state management and buffer validation

--- lua/ai_chat/ui.lua
+++ lua/ai_chat/ui.lua
@@ -1,23 +1,34 @@
 local M = {}
 local config = require("ai_chat.config")
 
-local chat_buf = nil
-local chat_win = nil
+local state = {
+	chat_buf = nil,
+	chat_win = nil,
+}
+
+local function is_valid(bufnr)
+	return bufnr and vim.api.nvim_buf_is_valid(bufnr)
+end
 
 function M.create_chat_window()
-	if chat_win and vim.api.nvim_win_is_valid(chat_win) then
-		vim.api.nvim_set_current_win(chat_win)
-		return
+	if state.chat_win and vim.api.nvim_win_is_valid(state.chat_win) then
+		vim.api.nvim_set_current_win(state.chat_win)
+		return state.chat_buf, state.chat_win
+	end
+
+	-- Create a new buffer if it doesn't exist
+	if not is_valid(state.chat_buf) then
+		state.chat_buf = vim.api.nvim_create_buf(false, true)
+
+		vim.api.nvim_buf_set_option(state.chat_buf, "filetype", "markdown")
+		vim.api.nvim_buf_set_option(state.chat_buf, "buftype", "nofile")
 	end
 
 	local width = math.floor(vim.o.columns * config.user_config.window.width)
 	local height = vim.o.lines - 3 -- leave some space for status line
 
-	-- Create buffer
-	chat_buf = vim.api.nvim_create_buf(false, true)
-
 	-- Create window
-	chat_win = vim.api.nvim_open_win(chat_buf, true, {
+	state.chat_win = vim.api.nvim_open_win(state.chat_buf, true, {
 		relative = "editor",
 		width = width,
 		height = height,
@@ -28,17 +39,24 @@ function M.create_chat_window()
 	})
 
 	-- Window settings
-	vim.wo[chat_win].wrap = true
-	vim.bo[chat_win].filetype = "markdown"
-	vim.bo[chat_win].buftype = "nofile"
+	vim.wo[state.chat_win].wrap = true
+	vim.wo[state.chat_win].number = false
+	vim.wo[state.chat_win].relativenumber = false
 
 	-- Key mappings
-	vim.api.nvim_buf_set_keymap(chat_buf, "n", "<CR>", ":lua require('ai_chat.chat_manager').send_message()<CR>", {
-		noremap = true,
-		silent = true,
-	})
+	vim.api.nvim_buf_set_keymap(
+		state.chat_buf,
+		"n",
+		"<CR>",
+		":lua require('ai_chat.chat_manager').send_message()<CR>",
+		{
+			noremap = true,
+			silent = true,
+		}
+	)
 
 	vim.api.nvim_command("startinsert")
+	return state.chat_buf, state.chat_win
 end
 
 return M
